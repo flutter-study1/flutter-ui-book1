f40f5b1baa4f2304fc8138214606b2abb6c2b41c
DaeLimIm <blackway76@gmail.com>
2022-01-26 00:19:57 +0900

    Chapter6 프로필 앱 만들기 2

---
+++ flutter_profile_copy/lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme(),
+      home: ProfilePage(),
+    );
+  }
+}
+
+
+class ProfilePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: ProfileDrawer(),
+      appBar: _buildProfileAppBar(),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          ProfileHeader(),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildProfileAppBar() {
+    return AppBar(
+      leading: Icon(Icons.arrow_back_ios),
+      title: Text("Profile"),
+      centerTitle: true,
+    );
+  }
+}
+
+
+class ProfileHeader extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 20),
+        _buildHeaderAvatar(),
+        SizedBox(width: 20),
+      ],
+    );
+  }
+
+  Widget _buildHeaderAvatar() {
+    return SizedBox(
+      width: 100,
+      height: 100,
+      child: CircleAvatar(
+        backgroundImage: AssetImage("assets/avatar.png"),
+      ),
+    );
+  }
+}
+
+
+
+class ProfileDrawer extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+const MaterialColor primaryWhite = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
+
+ThemeData theme() {
+  return ThemeData(
+    primarySwatch: primaryWhite,
+    appBarTheme: AppBarTheme(
+      iconTheme: IconThemeData(color: Colors.blue),
+    )
+  );
+}

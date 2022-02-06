ecce8e969bcc226e1826e9338fea55c766751e13
DaeLimIm <blackway76@gmail.com>
2022-02-06 21:03:34 +0900

    chapter-08 초안

--- flutter_shoppingcart_copy/README.md
+++ flutter_shoppingcart_copy/README.md
@@ -1,15 +1,63 @@
-# flutter_shoppingcart
+# 챕터 08 쇼핑카드 앱 만들기
+<br/>
 
-A new Flutter application.
 
-## Getting Started
+## 08 _ 1 앱 구조보기   
+<br/>
 
-This project is a starting point for a Flutter application.
+![ex_screenshot](./ppt_resources/1.png)
+![ex_screenshot](./ppt_resources/2.png)
 
-A few resources to get you started if this is your first Flutter project:
+## 08 _ 2 앱 플러터 상태 관리 StatefulWidget 
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+    1.StatefulWidget이란?
+        변수의 값(상태값)에 따라 화면을 다시 그릴 수도 있는 위젯.
+        StatelessWidget 는 Widget
+        StatefulWidget 는 Widget + State 
+        예)사진 액자에 사진 여러장이 돌아 가면서 나오는 것을 생각 하면 됨.
+        사진 + 액자 에서 사진 부분만 StatefulWidget 를 사용 하면 되겠죠!
+        StatefulWidget 는 setState 함수를 통해서 상태변수를 변경 하게 되면
+        build 함수가 다시 실행 되면서 그림이 다시 그려지게 됩니다.
+
+## 08 _ 3 앱 빼대 작성하기
+
+    1.프로젝트 구조 셋팅하기
+        -lib/components 패키지 생성, 그안에 필요한 소스를 분할 하여 넣음.
+        여러명이서 개발시 필요 또는 공통 기능이 구현해 다른 프로그램 개발시 사용이 필요한 소스는 분할 하는게 좋음.
+    2.이미지 파일등 필요한 것 assets/ 에 넣는다.
+
+## 08 _ 4 앱 만들어보기
+<br/>
+
+    1.AppBar _buildShoppingCartAppBar()
+    2.헤더 만들기 ShoppingCartHeader - setState() 함수
+    3.디테일 만들기 ShoppingCartDetail - Stack 위젯과 Positioned 위젯
+        -Stack 위젯은 여러 위젯을 겹치려는 경우에 사용, Postioned 위젯은 Stack 위젯과 함께 사용됨.
+    4.디테일 만들기 ShoppingCartDetail - CupertinoAlertDialog 위젯
+
+        |----------------------|
+        |                      | Add to Cart 클릭후.
+        | CupertinoAlertDialog-|---> Pop() Navigator.pop() 함수를 이용하여 Stack 제거
+        |                      |-> Stack
+        |----------------------|
+        | ShoppingCartpage     |-> 처음 화면. Add to Cart 클릭전. 
+        |----------------------|
+
+<br/>
+
+  
+### Mixin 이해해보기  
+<br/>
+
+
+- 믹스인은 프로그래머가 특정 코드를 다른 클래스에 삽입 할 수 있도록 하는 프로그래밍 개념입니다. 믹스인 프로그래밍은 특정 클래스에 작성된 기능들을 다른 클래스와 혼합하는 소프트웨어 개발 유형입니다.[5]
+
+믹스인 클래스는 필요로 하는 기능들을 포함하는 상위 클래스로서 역할을 합니다. 그리고 하위 클래스는 이 기능을 상속하거나 단순히 재사용 할 수 있습니다. 하지만 자식클래스를 특수화(Specialization)하는 수단으로는 사용할 수 없습니다. 일반적으로 믹스인은 엄격한 단일 "is-a"관계(상속관계)를 만들지 않고 원하는 기능을 하위 클래스로 전달합니다. 여기에 믹스인과 상속의 중요한 차이점이 있습니다. 자식클래스는 여전히 부모클래스의 모든 기능을 상속 할 수 있지만, 부모와 자식이라는 의미는 반드시 부여될 필요는 없습니다.
+
+[위키백과 ko.wikipedia.org/wiki/믹스인](https://ko.wikipedia.org/wiki/%EB%AF%B9%EC%8A%A4%EC%9D%B8)
+
+<br/>
+
 
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
--- flutter_shoppingcart_copy/lib/main.dart
+++ flutter_shoppingcart_copy/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_shoppingcart/components/shoppingcart_detail.dart';
-import 'package:flutter_shoppingcart/components/shoppingcart_header.dart';
+// import 'package:flutter_shoppingcart/components/shoppingcart_detail.dart';
+// import 'package:flutter_shoppingcart/components/shoppingcart_header.dart';
 // import 'package:flutter_shoppingcart/theme.dart';
+import 'dart:developer' as developer; //디버깅 로그 출력
 
 void main() {
   runApp(MyApp());
@@ -25,19 +27,20 @@ class ShoppingCartPage extends StatelessWidget {
       body: Column(
         children: [
           ShoppingCartHeader(),
-          // ShoppingCartDetail(),
+          ShoppingCartDetail(),
         ],
       ),
     );
   }
 
-
+// AppBar 영역.
   AppBar _buildShoppingCartAppBar() {
     return AppBar(
       leading: IconButton(
         icon: Icon(Icons.arrow_back),
         onPressed: () {},
       ),
+      // title: Text("앱바영역"),
       actions: [
         IconButton(
           icon: Icon(Icons.shopping_cart),
@@ -49,7 +52,6 @@ class ShoppingCartPage extends StatelessWidget {
     );
   }
 }
-
 
 const kPrimaryColor = MaterialColor(
   0xFFeeeeee,
@@ -70,7 +72,6 @@ const kPrimaryColor = MaterialColor(
 const kSecondaryColor = Color(0xFFc6c6c6); // 기본 버튼 색
 const kAccentColor = Color(0xFFff7643); // 활성화 버튼 색
 
-
 //ShoppingCartPage.start
 
 ThemeData theme() {
@@ -84,11 +85,164 @@ ThemeData theme() {
 
 //Component.start
 
+class ShoppingCartDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDetailNameAndPrice(),
+            _buildDetailRatingAndReviewCount(),
+            _buildDetailColorOptions(),
+            _buildDetailButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailButton(BuildContext context) {
+    return Align(
+      child: TextButton(
+        onPressed: () {
+          showCupertinoDialog(
+            // 1. 추가
+            context: context,
+            builder: (context) => CupertinoAlertDialog(
+              title: Text("장바구니에 담으시겠습니까?"),
+              actions: [
+                CupertinoDialogAction(
+                  child: Text("확인"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: kAccentColor,
+          minimumSize: Size(300, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Text(
+          "Add to Cart",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailColorOptions() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Color Options"),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              // 3. 동일한 색상 아이콘을 재사옹하기 위해 함수로 관리
+              _buildDetailIcon(Colors.black),
+              _buildDetailIcon(Colors.green),
+              _buildDetailIcon(Colors.orange),
+              _buildDetailIcon(Colors.grey),
+              _buildDetailIcon(Colors.white),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailIcon(Color mColor) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10),
+      // 5. Stack의 첫 번째 Container 위젯위에 Postioned 위젯이 올라가는 형태
+      child: Stack(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(),
+              shape: BoxShape.circle,
+            ),
+          ),
+          Positioned(
+            left: 5,
+            top: 5,
+            child: ClipOval(
+              child: Container(
+                color: mColor,
+                width: 40,
+                height: 40,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailRatingAndReviewCount() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          // 2. Spacer()로 Icon위젯과 Text위젯을 양끝으로 벌릴 수 있다. spaceBetween과 동일
+          Spacer(),
+          Text("review "),
+          Text("(26)", style: TextStyle(color: Colors.blue)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailNameAndPrice() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Urban Soft AL 10.0",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "\$699",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// StatefulWidget 사용하여 상태값에 따라 다시 화면을 그린다.
 class ShoppingCartHeader extends StatefulWidget {
   @override
   _ShoppingCartHeaderState createState() => _ShoppingCartHeaderState();
 }
 
+// selectedId 값을 때라 화면을 다시 그린다. build 함수를 다시 호출 하는 것임.
 class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
   int selectedId = 0;
 
@@ -101,10 +255,49 @@ class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
 
   @override
   Widget build(BuildContext context) {
+    developer.log("ShoppingCartHeader._ShoppingCartHeaderState.build 호출");
     return Column(
       children: [
         _buildDeaderPic(),
+        _buildHeaderSelector(),
       ],
+    );
+  }
+
+  Widget _buildHeaderSelector() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildheaderselectbutton(0, Icons.directions_bike),
+          _buildheaderselectbutton(1, Icons.motorcycle),
+          _buildheaderselectbutton(2, CupertinoIcons.car_detailed),
+          _buildheaderselectbutton(3, CupertinoIcons.airplane),
+        ],
+      ),
+    );
+  }
+
+  // 1. 다른 화면에서도 재사용하면 공통 컴포넌트 위젯으로 관리하는 것이 좋다.
+  Widget _buildheaderselectbutton(int id, IconData mIcon) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: id == selectedId ? kAccentColor : kSecondaryColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: IconButton(
+        icon: Icon(mIcon, color: Colors.black),
+        onPressed: () {
+          setState(() {
+            developer.log("setState 함수 호출 클릭 id=$id");
+            print("print : setState 함수 호출 클릭 id=$id");
+            selectedId = id;
+          });
+        },
+      ),
     );
   }
 
@@ -121,76 +314,5 @@ class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
     );
   }
 }
-
-// class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
-//   int selectedId = 0;
-//
-//   List<String> selectedPic = [
-//     "assets/p1.jpeg",
-//     "assets/p2.jpeg",
-//     "assets/p3.jpeg",
-//     "assets/p4.jpeg",
-//   ];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         _buildHeaderPic(),
-//         _buildHeaderSelector(),
-//       ],
-//     );
-//   }
-//
-//   Widget _buildHeaderPic() {
-//     return Padding(
-//       padding: const EdgeInsets.all(16.0),
-//       child: AspectRatio(
-//         aspectRatio: 5 / 3,
-//         child: Image.asset(
-//           selectedPic[selectedId],
-//           fit: BoxFit.cover,
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget _buildHeaderSelector() {
-//     return Padding(
-//       padding: const EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 30),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceAround,
-//         children: [
-//           _buildHeaderSelectorButton(0, Icons.directions_bike),
-//           _buildHeaderSelectorButton(1, Icons.motorcycle),
-//           _buildHeaderSelectorButton(2, CupertinoIcons.car_detailed),
-//           _buildHeaderSelectorButton(3, CupertinoIcons.airplane),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   // 1. 다른 화면에서도 재사용하면 공통 컴포넌트 위젯으로 관리하는 것이 좋다.
-//   Widget _buildHeaderSelectorButton(int id, IconData mIcon) {
-//     return Container(
-//       width: 70,
-//       height: 70,
-//       decoration: BoxDecoration(
-//         color: id == selectedId ? kAccentColor : kSecondaryColor,
-//         borderRadius: BorderRadius.circular(20),
-//       ),
-//       child: IconButton(
-//         icon: Icon(mIcon, color: Colors.black),
-//         onPressed: () {
-//           setState(() {
-//             selectedId = id;
-//           });
-//         },
-//       ),
-//     );
-//   }
-// }
-
-
 
 //Component.end
